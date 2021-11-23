49e7ff039557985dd24131a76e7e916697a5fb94
Markus <martve1214@skola.goteborg.se>
2021-11-23 11:41:15 +0100

    Bond between players

---
+++ Assets/DrawBond.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBond : MonoBehaviour
+{
+    LineRenderer lr;
+    public Transform[] linePositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < linePositions.Length; i++)
+        {
+            lr.SetPosition(i, linePositions[i].position);
+        }
+    }
+}
