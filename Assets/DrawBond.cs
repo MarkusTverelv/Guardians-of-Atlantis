4b43e547a591107702a19af52b7ed5cbd46f5843
CarlHag <90193755+CarlHag@users.noreply.github.com>
2021-11-24 11:06:07 +0100

    movement

--- Assets/DrawBond.cs
+++ Assets/DrawBond.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class DrawBond : MonoBehaviour
 {
     LineRenderer lr;
-    public Transform[] linePositions;
+    List<Transform> linePositions = new List<Transform>();
+
+    public bool visiable;
+    public int lineLenght;
+    public GameObject lineSegment;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         lr = GetComponent<LineRenderer>();
+        lr.positionCount = lineLenght;
+        List<GameObject> lineSegments = new List<GameObject>();
+        for(int i = 0; i<lineLenght; i++)
+        {
+            lineSegments.Add(Instantiate(lineSegment, transform.position + new Vector3(i / 10f, 0), Quaternion.identity, transform));
+            lineSegments[i].name = "Linesegment " + i;
+            lineSegments[i].GetComponent<SpriteRenderer>().enabled = visiable;
+            linePositions.Add(lineSegments[i].transform);
+            if (i != 0)
+                lineSegments[i].GetComponent<HingeJoint2D>().connectedBody = lineSegments[i - 1].GetComponent<Rigidbody2D>();
+            if (i == lineLenght / 2)
+                lineSegments[i].tag = "Player";
+        }
+        GameObject jellow = GameObject.Find("Jellow");
+        GameObject pinko  = GameObject.Find("Pinko");
+        jellow.GetComponent<HingeJoint2D>().connectedBody = lineSegments[0].GetComponent<Rigidbody2D>();
+        lineSegments[0].GetComponent<HingeJoint2D>().connectedBody = jellow.GetComponent<Rigidbody2D>();
+        jellow.transform.position = lineSegments[0].transform.position;
+        pinko.GetComponent<HingeJoint2D>().connectedBody = lineSegments[lineLenght-1].GetComponent<Rigidbody2D>();
+        pinko.transform.position = lineSegments[lineLenght - 1].transform.position;
         
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        for (int i = 0; i < linePositions.Length; i++)
+        for (int i = 0; i < linePositions.Count; i++)
         {
             lr.SetPosition(i, linePositions[i].position);
         }
