7442128d3d4da2f12b2a168e3b38e317d364c18f
Pontustorn <35564939+Pontustorn@users.noreply.github.com>
2021-12-01 10:31:37 +0100

    Push ability

--- Assets/PulseScript.cs
+++ Assets/PulseScript.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PulseScript : MonoBehaviour
 {
     List<GameObject> enemyList;
+    public Transform pulseTransform;
+    private float range;
+    private float rangeMax;
+    private bool canGrow = false;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyList = new List<GameObject>();
+        rangeMax = 4f;
     }
 
     // Update is called once per frame
@@ -16,34 +22,48 @@ public class PulseScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            addForce();
+            canGrow = true;
         }
+
+        if(canGrow)
+        {
+            float rangeSpeed = 8f;
+            range += rangeSpeed * Time.deltaTime;
+            if (range > rangeMax)
+            {
+                range = rangeMax;
+                Invoke("pulseEffect", 0.1f);
+                canGrow = false;
+            }
+
+            
+        }
+
+        pulseTransform.localScale = new Vector3(range, range);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
-            enemyList.Add(collision.gameObject);
-        Debug.Log(enemyList.Count);
+        {
+            addForce(collision);
+        }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-            enemyList.Remove(collision.gameObject);
-        Debug.Log(enemyList.Count);
-    }
-
-    private void addForce()
+    private void addForce(Collider2D collision)
     {
         for (int i = 0; i < 10; i++)
         {
-            foreach (GameObject l in enemyList)
-            {
-                Rigidbody2D rb = l.GetComponent<Rigidbody2D>();
-                Vector2 position = l.transform.position - this.transform.position;
+                Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+                Vector2 position = collision.transform.position - this.transform.position;
                 rb.AddForce(position, ForceMode2D.Impulse);
-            }
         }
     }
+
+    private void pulseEffect()
+    {
+        range = 0;
+    }
+
+
 }
