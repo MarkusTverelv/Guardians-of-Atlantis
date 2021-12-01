884cd7fb703d597f9a326943947c93cb25a078a8
Markus <martve1214@skola.goteborg.se>
2021-12-01 13:38:48 +0100

    Update YelloFollow.cs

--- Assets/YelloFollow.cs
+++ Assets/YelloFollow.cs
@@ -17,13 +17,14 @@ public class YelloFollow : MonoBehaviour
     private float angle = 0.0f;
     private float direction = 0.0f;
 
+    private bool canMove = false;
+
     private Rigidbody2D thisRigidbody2D;
     private Rigidbody2D parentRigidbody2D;
 
-    PlayerStates currentPlayerState = PlayerStates.Idle;
-
     private Vector3 offset;
-    private bool canMove = false;
+
+    PlayerStates currentPlayerState = PlayerStates.Idle;
 
     // Start is called before the first frame update
     void Start()
@@ -35,19 +36,29 @@ public class YelloFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction = Input.GetAxisRaw("Horizontal");
-        angle += direction * rotateSpeed * Time.deltaTime;
-        offset = new Vector3(Mathf.Sin(angle) * distance, Mathf.Cos(angle) * distance);
+        RotatePlayer();
 
+        #region PlayerState checks
         if (direction != 0)
             currentPlayerState = PlayerStates.Moving;
         else if (Input.GetKeyDown(KeyCode.Return))
             currentPlayerState = PlayerStates.Dash;
         else
             currentPlayerState = PlayerStates.Idle;
+        #endregion
 
         PlayerStateManager(currentPlayerState);
     }
+    private void RotatePlayer()
+    {
+        direction = Input.GetAxisRaw("Horizontal");
+        float currentAngle = Vector2.SignedAngle(parentRigidbody2D.position, thisRigidbody2D.position);
+        print(currentAngle);
+
+        angle += direction * rotateSpeed * Time.deltaTime;
+        offset = new Vector3(Mathf.Sin(angle) * distance, Mathf.Cos(angle) * distance);
+    }
+
     private void PlayerStateManager(PlayerStates current)
     {
         switch (current)
@@ -60,7 +71,7 @@ public class YelloFollow : MonoBehaviour
                 break;
             case PlayerStates.Dash:
                 canMove = true;
-                Dash(parentRigidbody2D.position - thisRigidbody2D.position);
+                Dash(thisRigidbody2D.position - parentRigidbody2D.position);
                 break;
             default:
                 break;
@@ -77,8 +88,9 @@ public class YelloFollow : MonoBehaviour
     {
         thisRigidbody2D.position = parentRigidbody2D.position + (Vector2)rotation;
     }
+
     private void Dash(Vector2 direction)
     {
-        thisRigidbody2D.AddForce(-direction.normalized * 100, ForceMode2D.Impulse);
+        thisRigidbody2D.AddForce(direction.normalized * 100, ForceMode2D.Impulse);
     }
 }
