5815d7a712c8afaa8c228d0cb6b02a3823438d4a
CarlHag <90193755+CarlHag@users.noreply.github.com>
2021-12-02 15:57:07 +0100

    enemy health

--- Assets/Scripts/JellyfishScript.cs
+++ Assets/Scripts/JellyfishScript.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class JellyfishScript : MonoBehaviour
 {
+    int health = 3;
+    int currentHealth;
     Rigidbody2D body;
     bool aggro, turn, patrolling;
     CircleCollider2D aggroRange;
     public float speed, agrroRange;
     GameObject player;
-    public AudioClip electricity;
+    public AudioClip electricity, hurt, death;
     AudioSource audioSource;
     SpriteRenderer spriteRenderer;
+    bool inv;
     
     Vector3 startPos;
     enum state 
@@ -26,8 +29,9 @@ public class JellyfishScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentHealth = health;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        audioSource = GetComponent<AudioSource>();
+        audioSource = GameObject.Find("Audio Source").GetComponent<AudioSource>();
         startPos = transform.position;
         transform.GetChild(0).GetComponent<CircleCollider2D>().radius = agrroRange;
         body = GetComponent<Rigidbody2D>();
@@ -80,14 +84,43 @@ public class JellyfishScript : MonoBehaviour
             currentState = state.returning;
         }
     }
+    IEnumerator flashRedTimer()
+    {
+        inv = true;
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(1f);
+        spriteRenderer.color = Color.white;
+        inv = false;
+    }
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<PlayerScript>().hurt();
-               
+
+        }
+        else if (collision.gameObject.CompareTag("Projectile")&&!inv)
+        { 
+            if(currentHealth>1)
+            {
+                StartCoroutine(flashRedTimer());
+                Debug.Log(currentHealth);
+                currentHealth--;
+                audioSource.pitch = 1;
+                audioSource.PlayOneShot(hurt);
+            }
+            else
+            {
+                audioSource.pitch = 1;
+                audioSource.PlayOneShot(death);
+                Destroy(gameObject);
+                audioSource.pitch = 1;
+
+            }    
         }
             
+            
     }
+
 
 }
