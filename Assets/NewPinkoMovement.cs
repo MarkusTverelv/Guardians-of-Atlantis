585af125de9a36b8a528a689a4fdd9b43667e103
Markus <martve1214@skola.goteborg.se>
2021-12-07 14:39:04 +0100

    Shoot for new movement

--- Assets/NewPinkoMovement.cs
+++ Assets/NewPinkoMovement.cs
@@ -17,6 +17,15 @@ public class NewPinkoMovement : MonoBehaviour
     [SerializeField]
     private Transform pinkoGFXTransform;
 
+    public Transform PinkoGFX
+    {
+        get { return pinkoGFXTransform; }
+        set
+        {
+            pinkoGFXTransform = value;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/AltMovement.cs
+++ Assets/Scripts/AltMovement.cs
@@ -22,8 +22,14 @@ public class AltMovement : MonoBehaviour
     Rigidbody2D yelloRigidbody;
     Rigidbody2D pinkoRigidbody;
 
-    private float distance = 0.0f;
-    private Vector3 midPoint;
+    Vector3 midPoint;
+
+    float distance = 0.0f;
+    float shootForce = 50.0f;
+
+    bool shoot = false;
+
+    NewPlayerStates currentState = NewPlayerStates.Idle;
 
     // Start is called before the first frame update
     void Start()
@@ -38,8 +44,13 @@ public class AltMovement : MonoBehaviour
     }
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+            currentState = NewPlayerStates.Attack;
+
         yelloMove.Turn();
         pinkoMove.Turn();
+
+        StateSwitcher();
     }
 
     //M=(2x1​+x2​​,2y1​+y2​​)
@@ -49,15 +60,63 @@ public class AltMovement : MonoBehaviour
         distance = (yelloRigidbody.position - pinkoRigidbody.position).magnitude;
         midPoint = (yelloRigidbody.position + pinkoRigidbody.position) / 2;
 
-        Attraction();
-
         yelloMove.Move();
         pinkoMove.Move();
+
     }
 
     private void Attraction()
     {
         yelloRigidbody.position = Vector3.Slerp(yelloRigidbody.position, midPoint, (distance / 10) * Time.deltaTime);
         pinkoRigidbody.position = Vector3.Slerp(pinkoRigidbody.position, midPoint, (distance / 10) * Time.deltaTime);
+    }
+
+    private void StateSwitcher()
+    {
+        switch (currentState)
+        {
+            case NewPlayerStates.Idle:
+                Attraction();
+                break;
+            case NewPlayerStates.Moving:
+                Attraction();
+                break;
+            case NewPlayerStates.Shield:
+                break;
+            case NewPlayerStates.Attack:
+                Shoot(shoot);
+                break;
+            case NewPlayerStates.Push:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Shoot(bool canShoot)
+    {
+        if (distance >= 1.5f)
+            yelloRigidbody.position = Vector2.MoveTowards(yelloRigidbody.position, pinkoRigidbody.position, 50 * Time.deltaTime);
+
+        else if (distance < 1.5f)
+        {
+            if (!canShoot)
+                yelloRigidbody.position = pinkoRigidbody.position;
+        }
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            shootForce += 0.2f;
+
+            if (shootForce > 300.0f)
+                shootForce = 300.0f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            yelloRigidbody.AddForce(pinkoMove.PinkoGFX.up * shootForce, ForceMode2D.Impulse);
+            shootForce = 50.0f;
+            currentState = NewPlayerStates.Idle;
+        }
     }
 }
