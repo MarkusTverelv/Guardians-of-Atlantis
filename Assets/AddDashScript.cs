4a8100a11a349d142fd6c88ad07824f7202189b8
Pontustorn <35564939+Pontustorn@users.noreply.github.com>
2021-12-20 15:29:58 +0100

    levelfix

--- Assets/AddDashScript.cs
+++ Assets/AddDashScript.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddDashScript : MonoBehaviour
 {
     PlayerSharedScript pss;
+    public static bool doIExist = true;
+    public GameObject yellowExplosion;
+    public Text dashText;
     // Start is called before the first frame update
     void Start()
     {
+        if (!doIExist)
+        {
+            gameObject.active = false;
+        }
+
         pss = GameObject.Find("NewPlayers").GetComponent<PlayerSharedScript>();
     }
 
@@ -20,6 +29,10 @@ public class AddDashScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         pss.addDash();
+        doIExist = false;
+        dashText.GetComponent<TextScript>().textAnimate();
+        Instantiate(yellowExplosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        
     }
 }
