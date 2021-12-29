2db07f9207a3bfc3969ebdb644d65a51282516c0
Pontustorn <35564939+Pontustorn@users.noreply.github.com>
2021-12-29 20:20:03 +0100

    löste nån skit jag trodde funkade dashGem/lifeGem

--- Assets/Scripts/LevelScripts/AddDashScript.cs
+++ Assets/Scripts/LevelScripts/AddDashScript.cs
@@ -8,8 +8,14 @@ public class AddDashScript : MonoBehaviour
     AudioScript audioScript;
     PlayerSharedScript pss;
     public static bool doIExist = true;
+    public static bool doIExist2 = true;
+    public static bool doIExist3 = true;
     public GameObject yellowExplosion;
     public Text dashText;
+    public int dashNmbr;
+    public GameObject dashGem;
+    public GameObject dashGem2;
+    public GameObject dashGem3;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +23,20 @@ public class AddDashScript : MonoBehaviour
 
         if (!doIExist)
         {
-            gameObject.active = false;
+            dashGem.GetComponent<SpriteRenderer>().enabled = false;
+            dashGem.GetComponent<CircleCollider2D>().enabled = false;
         }
-        GetComponent<SpriteRenderer>().enabled = false;
+        if (!doIExist2)
+        {
+            dashGem2.GetComponent<SpriteRenderer>().enabled = false;
+            dashGem2.GetComponent<CircleCollider2D>().enabled = false;
+        }
+        if (!doIExist3)
+        {
+            dashGem3.GetComponent<SpriteRenderer>().enabled = false;
+            dashGem3.GetComponent<CircleCollider2D>().enabled = false;
+        }
+
 
         pss = GameObject.Find("NewPlayers").GetComponent<PlayerSharedScript>();
     }
@@ -27,17 +44,38 @@ public class AddDashScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        audioScript.playDashSound();
-        pss.AddDash();
-        doIExist = false;
-        dashText.GetComponent<TextScript>().textAnimate();
-        Instantiate(yellowExplosion, transform.position, Quaternion.identity);
-        Destroy(gameObject);
-        
+        if (dashNmbr == 1)
+        {
+            audioScript.playDashSound();
+            pss.AddDash();
+            doIExist = false;
+            dashText.GetComponent<TextScript>().textAnimate();
+            Instantiate(yellowExplosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        if (dashNmbr == 2)
+        {
+            audioScript.playDashSound();
+            pss.AddDash();
+            doIExist2 = false;
+            dashText.GetComponent<TextScript>().textAnimate();
+            Instantiate(yellowExplosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        if (dashNmbr == 3)
+        {
+            audioScript.playDashSound();
+            pss.AddDash();
+            doIExist3 = false;
+            dashText.GetComponent<TextScript>().textAnimate();
+            Instantiate(yellowExplosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
     }
 }
